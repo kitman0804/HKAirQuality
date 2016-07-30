76e6aa635fa8b9f729195fbcf2a90943a495bf72
kitman0804 <kitman0804@gmail.com>
2016-07-30 20:57:00 +0800

    Version 0.1.3
    change delimiter from ',' to '\t' in .tsv

--- tidy_csv.py
+++ tidy_csv.py
@@ -2,29 +2,35 @@ import os
 import re
 import pandas as pd
 
+
 # All downloaded csv
 fileName = os.listdir("rawdata")
 station = [re.sub("AIR_[0-9]{4}_(.*)\.csv", "\\1", x) for x in fileName]
 station = pd.unique(station)
 
+
 # Read downloaded csv
-def read_air_quality(x):
+def read_raw(x):
     file = pd.read_csv(os.path.join("rawdata", x), 
                        skiprows=11, na_values="N.A.")
     file["DATE"] = pd.to_datetime(file.DATE, format="%d/%m/%Y")
     print("%s was read." % (x))
     return file
 
+
+def group_raw(station, rawFolder="rawdata", dataFolder="data"):
+    fileName = os.listdir(rawFolder)
+    files = [read_raw(x) for x in fileName if re.sub("AIR_[0-9]{4}_(.*)\.csv", "\\1", x) == s]
+    airQuality = pd.concat(files, axis=0)
+    airQuality = airQuality.sort_values(by=["STATION", "DATE", "HOUR"])
+    path = os.path.join(dataFolder, "air_quality_%s.tsv" % s)
+    airQuality.to_csv(path, sep="\t", index=False)
+    print("Data of %s station was saved." % s)
+
+
 # Combine data by station
 for s in station:
-    files = [x for x in fileName if re.sub("AIR_[0-9]{4}_(.*)\.csv", "\\1", x) == s]
-    files = [read_air_quality(x) for x in files]
-    air_quality = pd.concat(files, axis=0)
-    air_quality = air_quality.sort_values(by=["STATION", "DATE", "HOUR"])
-    path = "data/air_quality_%s.tsv" % s
-    air_quality.to_csv(path, index=False)
-    print("Data of %s station was saved." % s)
-    del files, air_quality, path
+    group_raw(station=s)
 
 
 
